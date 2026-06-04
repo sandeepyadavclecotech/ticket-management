42896fbe8bf17c4e270fda17050c22623f423267
sandeepyadavclecotech <sandeep.yadav@clecotech.com>
2026-06-04 14:53:38 +0530

    devise.rb env fixes

--- db/migrate/20260604091035_add_username_and_phone_no_to_customers.rb
+++ db/migrate/20260604091035_add_username_and_phone_no_to_customers.rb
@@ -1,6 +1,4 @@
 class AddUsernameAndPhoneNoToCustomers < ActiveRecord::Migration[7.1]
   def change
-    add_column :customers, :username, :string
-    add_column :customers, :phone_no, :string
   end
 end
