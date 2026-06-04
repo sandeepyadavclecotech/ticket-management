7bf978d7abd6c362a3879ec9c479a2f3d9b1a8f8
sandeepyadavclecotech <sandeep.yadav@clecotech.com>
2026-06-04 14:34:08 +0530

    added username and phone number field to signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :phone_no])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :phone_no])
+  end
 end
